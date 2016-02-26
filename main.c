70756bac43c2af408c1161f5854ee5c35c034696
zwertv_e <elliott.zwertvaegher@epitech.eu>
2016-02-26 15:43:40 +0100

    'autopush'

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ void my_putchar(char c)
 int	main()
 {
   //Bonjour, C'est Alaric qui m'a dit de faire ça.
-  //Bonjour, C'est Nicolas qui m'a dit de faire ça.
   my_putchar("mdr");
   return (1);
 }
