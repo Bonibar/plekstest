32a4c746931f9d5d5d818ab5d2634c45d036c579
unknown <Nicolas Ribeiro>
2016-02-26 16:25:49 +0100

    Bonjour

--- main.c
+++ main.c
@@ -8,9 +8,7 @@ void my_putchar(char c)
 int	main()
 {
   //Bonjour, C'est Alaric qui m'a dit de faire ça.
-  //Voici la pull request
-  //Voilà la pull request qui fait défaut à toute la planète :)
   my_putchar("Bonjour le FORWARD");
-  return (-42); // C'est ici que j'ai ajouté du code Hihihihi :)
+  return (-42);
 }
 
